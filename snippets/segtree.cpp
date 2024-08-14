eca7f0948197d470fe326dd5172ba98babc9d55e
Chengke Wang <chengke@pku.edu.cn>
2024-08-14 10:45:18 +0800

    update segtree

--- snippets/segtree.cpp
+++ snippets/segtree.cpp
@@ -4,79 +4,76 @@
 using namespace std;
 
 /* Snippet BEGIN */
-struct MinLazy
+
+struct NullLazy
 {
-    int lazy;
-    void clear() { lazy = 0; }
-    void apply(const MinLazy &other) { lazy += other.lazy; }
+    void clear() {}
+    void apply(const NullLazy &) {}
 };
 
-struct MinInfo
-{
-    int min = 0;
-    int min_count = 0;
-
-    MinInfo merge(const MinInfo &other) const
-    {
-        auto &self = *this;
-        MinInfo ret{
-            std::min(self.min, other.min),
-            0,
-        };
-        if (self.min == ret.min)
-            ret.min_count += self.min_count;
-        if (other.min == ret.min)
-            ret.min_count += other.min_count;
-        return ret;
-    }
-
-    void apply(const MinLazy &lazy) { min += lazy.lazy; }
-
-    int countZeros()
-    {
-        assert(min >= 0);
-        return min == 0 ? min_count : 0;
-    }
-};
-
-template <typename Info, typename Lazy> class SegmentTree
+template <typename Info, typename Lazy = NullLazy> class SegmentTree
 {
   public:
-    void init(int l, int r, std::function<Info(int)> map)
+    void init(
+        int l, int r, std::function<Info(int)> map = [](int) { return Info(); })
     {
         auto n_ = r - l + 1;
         root_l = l;
         root_r = r;
         nodes.assign(4 << std::__lg(n_), Node());
-        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+
+        auto build = [&](int p, int l, int r, auto &&self) -> void {
             nodes[p].lazy.clear();
             if (l == r) {
                 nodes[p].info = map(l);
                 return;
             }
-            int m = (l + r) / 2;
-            build(2 * p, l, m);
-            build(2 * p + 1, m + 1, r);
+            int m = l + (r - l) / 2;
+            self(2 * p, l, m, self);
+            self(2 * p + 1, m + 1, r, self);
             pull(p);
         };
-        build(1, l, r);
+
+        build(1, l, r, build);
     }
+
+    void modify(int p, const Info &v) { modify(1, root_l, root_r, p, v); }
 
     void modify(int p, const Lazy &v) { modify(1, root_l, root_r, p, p, v); }
 
     void modify(int l, int r, const Lazy &v)
     {
-        if (l > r) return;
+        if (l > r)
+            return;
         assert(l <= r);
         assert(l >= root_l && r <= root_r);
         modify(1, root_l, root_r, l, r, v);
     }
 
-    Info rangeQuery() { return rangeQuery(1, root_l, root_r, root_l, root_r); }
-
-    Info rangeQuery(int l, int r)
+    Info range_query()
     {
-        return rangeQuery(1, root_l, root_r, l, r);
+        return range_query(1, root_l, root_r, root_l, root_r);
+    }
+
+    Info range_query(int l, int r)
+    {
+        assert(l <= r);
+        return range_query(1, root_l, root_r, l, r);
+    }
+
+    static constexpr int NOT_FOUND = INT_MIN;
+
+    template <class F>
+        requires std::predicate<F, Info>
+    int find_first_idx(int x, int y, F &&pred)
+    {
+        return find_idx<true>(1, root_l, root_r, x, y, std::forward<F>(pred));
+    }
+    template <class F>
+        requires std::predicate<F, Info>
+    int find_last_idx(int x, int y, F &&pred)
+    {
+        return find_idx<false>(1, root_l, root_r, x, y, std::forward<F>(pred));
     }
 
   private:
@@ -111,7 +108,7 @@ template <typename Info, typename Lazy> class SegmentTree
             return;
         }
         push(p);
-        int m = (l + r) / 2;
+        int m = l + (r - l) / 2;
         if (m >= x) {
             modify(2 * p, l, m, x, y, v);
         }
@@ -121,42 +118,149 @@ template <typename Info, typename Lazy> class SegmentTree
         pull(p);
     }
 
-    Info rangeQuery(int p, int l, int r, int x, int y)
+    void modify(int p, int l, int r, int x, const Info &v)
+    {
+        if (l == r) {
+            nodes[p].info = v;
+            return;
+        }
+        int m = l + (r - l) / 2;
+        if (x <= m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m + 1, r, x, v);
+        }
+        pull(p);
+    }
+
+    Info range_query(int p, int l, int r, int x, int y)
     {
         if (l >= x && r <= y) {
             return nodes[p].info;
         }
         push(p);
-        int m = (l + r) / 2;
+        int m = l + (r - l) / 2;
 
         bool qleft = m >= x;
         bool qright = m + 1 <= y;
         assert(qleft || qright);
         if (qleft && qright) {
-            auto left = rangeQuery(2 * p, l, m, x, y);
-            auto right = rangeQuery(2 * p + 1, m + 1, r, x, y);
+            auto left = range_query(2 * p, l, m, x, y);
+            auto right = range_query(2 * p + 1, m + 1, r, x, y);
             return left.merge(right);
         }
 
         if (qleft) {
-            return rangeQuery(2 * p, l, m, x, y);
+            return range_query(2 * p, l, m, x, y);
         } else {
-            return rangeQuery(2 * p + 1, m + 1, r, x, y);
+            return range_query(2 * p + 1, m + 1, r, x, y);
         }
     }
+
+    template <bool Forward, class F>
+        requires std::predicate<F, Info>
+    int find_idx(int p, int l, int r, int x, int y, F &&pred)
+    {
+        if (l > y || r < x) {
+            return NOT_FOUND;
+        }
+        if (l >= x && r <= y && !pred(nodes[p].info)) {
+            return NOT_FOUND;
+        }
+        if (l == r) {
+            return l;
+        }
+        int m = l + (r - l) / 2;
+        int res = NOT_FOUND;
+        if constexpr (Forward) {
+            res = find_idx<Forward>(2 * p, l, m, x, y, pred);
+            if (res == NOT_FOUND) {
+                res = find_idx<Forward>(2 * p + 1, m + 1, r, x, y, pred);
+            }
+        } else {
+            res = find_idx<Forward>(2 * p + 1, m + 1, r, x, y, pred);
+            if (res == NOT_FOUND) {
+                res = find_idx<Forward>(2 * p, l, m, x, y, pred);
+            }
+        }
+        return res;
+    }
+};
+
+struct AddLazy
+{
+    int lazy;
+    void clear() { lazy = 0; }
+    void apply(const AddLazy &other) { lazy += other.lazy; }
+};
+
+struct MinCountInfo
+{
+    int min = 0;
+    int min_count = 0;
+
+    MinCountInfo merge(const MinCountInfo &other) const
+    {
+        auto &self = *this;
+        MinCountInfo ret{
+            std::min(self.min, other.min),
+            0,
+        };
+        if (self.min == ret.min)
+            ret.min_count += self.min_count;
+        if (other.min == ret.min)
+            ret.min_count += other.min_count;
+        return ret;
+    }
+
+    void apply(const AddLazy &lazy) { min += lazy.lazy; }
+
+    int count_zeros()
+    {
+        assert(min >= 0);
+        return min == 0 ? min_count : 0;
+    }
+};
+
+template <typename T, typename Comparator = std::less<T>> struct CmpInfo
+{
+    T value;
+
+    CmpInfo merge(const CmpInfo &other) const
+    {
+        // Return the max value like a priority queue
+        if (Comparator()(value, other.value))
+            return other;
+        return *this;
+    }
+
+    void apply(const NullLazy &) {}
 };
 
 int segtree_example()
 {
-    SegmentTree<MinInfo, MinLazy> s;
-    int n = 10;
-    s.init(1, n, []([[maybe_unused]] int i) { return MinInfo{0, 1}; });
-    cout << s.rangeQuery().countZeros() << endl;
-    cout << s.rangeQuery(2, 5).countZeros() << endl;
-    s.modify(2, 6, {1});
-    cout << s.rangeQuery().countZeros() << endl;
-    s.modify(3, 4, {-1});
-    cout << s.rangeQuery().countZeros() << endl;
+    {
+        SegmentTree<MinCountInfo, AddLazy> s;
+        int n = 10;
+        s.init(1, n, []([[maybe_unused]] int i) { return MinCountInfo{0, 1}; });
+        cout << s.range_query().count_zeros() << endl;
+        cout << s.range_query(2, 5).count_zeros() << endl;
+        s.modify(2, 6, {1});
+        cout << s.range_query().count_zeros() << endl;
+        s.modify(3, 4, {-1});
+        cout << s.range_query().count_zeros() << endl;
+    }
+
+    {
+        SegmentTree<CmpInfo<int>> s;
+        s.init(-5, 5);
+        s.modify(-2, CmpInfo{3});
+        assert(s.find_first_idx(
+                   -4, 0, [](CmpInfo<int> x) { return x.value >= 3; }) == -2);
+        assert(s.find_first_idx(-4, -3, [](CmpInfo<int> x) {
+            return x.value >= 3;
+        }) == s.NOT_FOUND);
+    }
 
     return 0;
 }
